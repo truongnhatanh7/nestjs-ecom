611c61195df151b37a3f6c9f0150c3759b32006b
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-08-21 11:07:20 +0700

    feat: add secu guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ProductModule } from './product/product.module';
 import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/store'),
     ProductModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [ProductController],
   providers: [ProductService],
--- src/user/schemas/user.schem.ts
+++ src/user/schemas/user.schem.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-// import { Role } from 'src/auth/enums/role.enum';
+import { Role } from 'src/auth/enums/role.enum';
 
 export type UserDocument = User & Document;
 
@@ -15,10 +15,8 @@ export class User {
   @Prop()
   password: string;
 
-  /*
   @Prop()
   roles: Role[];
-*/
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
