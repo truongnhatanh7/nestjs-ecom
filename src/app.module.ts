8f872497a7aaec9b002f428221834499d8451c4a
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-08-21 11:35:36 +0700

    fix: invalid product injection to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
-import { ProductController } from './product/product.controller';
-import { ProductService } from './product/product.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/store'),
+    MongooseModule.forRoot('mongodb://localhost:27017/store'),
     ProductModule,
     UserModule,
     AuthModule,
     CartModule,
   ],
-  controllers: [ProductController],
-  providers: [ProductService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
