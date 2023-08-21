4107109087f5967a9be8ceb97e3e0a522a8f9f7f
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-08-21 10:44:09 +0700

    feat: add product route

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { FilterProductDTO } from './dtos/filter-product.dto';
+import { CreateProductDTO } from './dtos/create-product.dto';
 
-@Controller('product')
-export class ProductController {}
+@Controller('store/products')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Get('/')
+  async getProducts(@Query() filterProductDTO: FilterProductDTO) {
+    if (Object.keys(FilterProductDTO).length) {
+      return await this.productService.getFilteredProducts(filterProductDTO);
+    }
+    return await this.productService.getAllProducts();
+  }
+
+  @Get('/:id')
+  async getProduct(@Param('id') id: string) {
+    const product = await this.productService.getProduct(id);
+    if (!product) throw new NotFoundException('Product not found');
+    return product;
+  }
+
+  @Post('/:id')
+  async addProduct(@Body() createProductDTO: CreateProductDTO) {
+    const product = await this.productService.addProduct(createProductDTO);
+    return product;
+  }
+
+  @Put('/:id')
+  async updateProduct(
+    @Param('id') id: string,
+    @Body() createProductDTO: CreateProductDTO,
+  ) {
+    const product = await this.productService.updateProduct(
+      id,
+      createProductDTO,
+    );
+    if (!product) throw new NotFoundException('Product does not exist!');
+    return product;
+  }
+
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string) {
+    const product = await this.productService.deleteProduct(id);
+    if (!product) throw new NotFoundException('Product does not exist');
+    return product;
+  }
+}
